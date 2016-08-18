149be5747a920758d1a69e94cbe98b9c9b9921ae
Neal Donnelly <NealJMD@gmail.com>
2016-08-18 17:26:49 -0600

    remove all javascript left in Champaign repo, rename and manifest remaining

---
+++ javascripts/champaign.js
@@ -0,0 +1,7 @@
+require("javascripts/check_top");
+require("javascripts/ga_event_reporting");
+
+window.sumofus = window.sumofus || {};
+window.sumofus.ActionCounter = require('javascripts/backbone/action_counter');
+window.sumofus.DesktopSticky = require('javascripts/backbone/desktop_sticky');
+window.sumofus.SimpleCarousel = require('javascripts/backbone/simple_carousel');
--- javascripts/selectize_config.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-  $('.selectize-container').selectize({
-    plugins: ['remove_button'],
-    closeAfterSelect: true
-  });
-});
