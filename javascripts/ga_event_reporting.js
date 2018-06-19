15042900b43ba76822182c1aed91cacd4f1ab9d9
Vince Martinez <eykosioux@gmail.com>
2018-06-19 17:03:39 +0100

    Migrate from $.subscribe to window.event.on

--- javascripts/ga_event_reporting.js
+++ javascripts/ga_event_reporting.js
@@ -1,16 +1,16 @@
-$(function() {
+if (window.event) {
   const EVENTS_TO_REPORT = [
     'direct_debit:opened',
     'direct_debit:donated_via_other',
     'direct_debit:donated',
   ];
 
-  $.subscribe(EVENTS_TO_REPORT.join(' '), function(ev) {
-    let splitted = ev.type.split(':');
-    let eventCategory = splitted.length > 1 ? splitted[0] : 'generic';
-    let eventAction = splitted.length > 1 ? splitted[1] : splitted[0];
-    if (typeof window.ga === 'function') {
-      window.ga('send', 'event', eventCategory, eventAction);
-    }
+  EVENTS_TO_REPORT.forEach(e => {
+    const [category, action] = e.split(':');
+    window.event.on(e, () => {
+      if (typeof window.ga === 'function') {
+        window.ga('send', 'event', category, action);
+      }
+    })
   });
-});
+}
