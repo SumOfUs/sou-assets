97d9774a789442c53a1999638c61479ee8bbc5ae
Vince Martinez <eykosioux@gmail.com>
2019-08-06 16:42:21 +0100

    Remove JavaScript sources from sou-assets

--- javascripts/champaign.js
+++
@@ -1,7 +0,0 @@
-require("./check_top");
-require("./ga_event_reporting");
-
-window.champaign = window.champaign || {};
-window.champaign.ActionCounter = require('./backbone/action_counter');
-window.champaign.DesktopSticky = require('./backbone/desktop_sticky');
-window.champaign.SimpleCarousel = require('./backbone/simple_carousel');
